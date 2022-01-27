0467de72622df2a72e98d694d5541d41c7845529
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2022-01-27 14:42:16 -0500

    attempted to add explosion
    it doesnt work lmao

--- Circle.java
+++ Circle.java
@@ -5,8 +5,11 @@ import java.awt.Rectangle;
 
 public class Circle {
 	
+	boolean deploded = false;
+	
 	public int radius;
 	public Point center;
+	public int x, y;
 	public Color color;
 	
 	public double angle = 0;
@@ -22,8 +25,10 @@ public class Circle {
 		this.angle = Math.toRadians(angle);
 		this.slope = Math.tan(angle);
 		
-		//generate target point
+		this.x = center.x;
+		this.y = center.y;
 		
+		//generate target point
 		int speed = 10;
 		
 		//cos and sin
@@ -34,18 +39,54 @@ public class Circle {
 		dy = speed * Math.sin(angle);
 	}
 	
+	public static double dist(int x1, int y1, int x2, int y2) {
+		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+	public static double dist(Point a, Point b) {
+		return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+	}
+	
+	public static boolean colliding(int x1, int y1, int r1, int x2, int y2, int r2) {
+		return dist(x1, y1, x2, y2) <= r1 + r2;
+	}
+	
+	public static boolean colliding(Point a, int r1, Point b, int r2) {
+		return dist(a, b) < r1 + r2;
+	}
+	
 	void draw(Graphics2D g2) {
 		
 		Rectangle bounds = new Rectangle(0, 0, Game.PREF_W, Game.PREF_H);
 		
 		center.x += dx;
 		center.y += dy;
-		if ((center.x - radius + dx < 0 - radius) || (center.x + radius + dx > bounds.width))
+		
+		x = center.x;
+		y = center.y;
+		
+		if(dx == 0 && dy == 0) {
+			
+			if(radius < Game.maxRadius && !deploded) {				
+				radius++;
+				x = center.x;
+				y = center.y;
+			}
+			if(radius >= Game.maxRadius) {
+				deploded = true;
+			}
+			if(radius > 0 && deploded) {
+				radius--;
+				x = center.x;
+				y = center.y;
+			}
+		}
+		
+		if ((center.x - radius + dx < 0 - radius / 2) || (center.x + radius + dx > bounds.width + radius / 2))
 		dx = -dx;
-		if ((center.y - radius + dy < 0 - radius) || (center.y + radius + dy > bounds.height))
+		if ((center.y - radius + dy < 0 - radius / 2) || (center.y + radius + dy > bounds.height + radius / 2))
 		dy = -dy;
 		g2.setColor(color);
-		g2.fillOval(center.x, center.y, radius, radius);
+		g2.fillOval(x - radius / 2,y - radius / 2,radius,radius);
 	}
 	
 }
--- Game.java
+++ Game.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
@@ -27,41 +28,62 @@ public class Game extends JPanel implements MouseListener {
 	public static final int PREF_W = 1111;
 	public static final int PREF_H = 696;
 
-	public ArrayList<Circle> circles = new ArrayList<Circle>();
+	public static ArrayList<Circle> circles = new ArrayList<Circle>();
+	public static ArrayList<Circle> blownUpCircles = new ArrayList<Circle>();
+
+	// player clicked circle
+	public boolean clicked = false;
+	public boolean deploded = false;
+	public static int circleX = 100000;
+	public static int circleY = 100000;
+	public static int circleR = 0;
 	
-	//player clicked circle
+	Color circleColor = (new Color(rand(0, 255), rand(0, 255), rand(0, 255), 127));
 	
-	boolean clicked = false;
-	int circleX;
-	int circleY;
-	int circleR;
-	Color circleColor = (new Color(rand(0,255), rand(0,255), rand(0,255), 127));
+	public static int maxRadius = 150;
 
 	public Timer timer = new Timer(1000 / 60, new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			repaint();
-			if(clicked) {
+			if(clicked && circleR < maxRadius && !deploded) {
 				circleR++;
-				circleX--;
-				circleY--;
+			}
+			
+			if(circleR == maxRadius) {
+				deploded = true;
+			}
+			
+			if(deploded) {
+				circleR--;
+				if(circleR <= 0) {
+					clicked = false;
+					deploded = false;
+				}
+			}
+			
+			if(clicked) {
+				for(int i = 0; i < circles.size(); i++) {
+					if(Circle.colliding(new Point(circleX + circleR / 2, circleY + circleR / 2), circleR, new Point(circles.get(i).x + circles.get(i).radius / 2, circles.get(i).y + circles.get(i).radius / 2), circles.get(i).radius)) {
+						circles.get(i).dx = 0;
+						circles.get(i).dy = 0;
+					}
+				}
 			}
 		}
 	});
-	
+
 	// Class constructor BOB tHE BUILDER CAN hE FIX IT?????T?T??/
 	public Game() {
 		this.setFocusable(true);
 		this.setBackground(Color.WHITE);
 		this.addMouseListener(this);
-		
+
 		timer.start();
-		
-		for(int i = 0; i < 10; i++) {
-			circles.add(new Circle(rand(100, PREF_W - 100),
-					rand(100, PREF_H - 100),
-					100,
-					new Color(rand(0,255), rand(0,255), rand(0,255), 127), rand(-180, 180)));
-			if(circles.get(i).dx == 0 && circles.get(i).dy == 0) {
+
+		for (int i = 0; i < 10; i++) {
+			circles.add(new Circle(rand(100, PREF_W - 100), rand(100, PREF_H - 100), 100,
+					new Color(rand(0, 255), rand(0, 255), rand(0, 255), 127), rand(-180, 180)));
+			if (circles.get(i).dx == 0 && circles.get(i).dy == 0) {
 				circles.get(i).dx = 1;
 			}
 		}
@@ -72,26 +94,27 @@ public class Game extends JPanel implements MouseListener {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 
-		g2.setRenderingHint(
-		        RenderingHints.KEY_TEXT_ANTIALIASING,
-		        RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
 
-		if(clicked) {
+		if (clicked) {
 			g2.setColor(circleColor);
-			g2.fillOval(circleX + circleR / 2, circleY + circleR / 2, circleR, circleR);
+			g2.fillOval(circleX - circleR / 2,circleY - circleR / 2,circleR,circleR);
+			g2.setColor(Color.BLACK);
+			g2.setStroke(new BasicStroke(3));
+			g2.drawOval(circleX - maxRadius / 2, circleY - maxRadius / 2, maxRadius, maxRadius);
 		}
-		
-		for(int i = 0; i < circles.size(); i++) {
+
+		for (int i = 0; i < circles.size(); i++) {
 			circles.get(i).draw(g2);
 		}
 	}
-	
+
 	public int rand(int min, int max) {
-		return (int) Math.floor(Math.random()*(max-min+1)+min);
+		return (int) Math.floor(Math.random() * (max - min + 1) + min);
 	}
-	
+
 	public double drand(int min, int max) {
-		return Math.random()*(max-min+1)+min;
+		return Math.random() * (max - min + 1) + min;
 	}
 
 	/** ******* METHODS FOR INITIALLY CREATING THE JFRAME AND JPANEL *********/
@@ -109,8 +132,6 @@ public class Game extends JPanel implements MouseListener {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		
 
 	}
 
@@ -125,10 +146,12 @@ public class Game extends JPanel implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		System.out.println("OMG YOU JUST CLICKED WTF");
-		circleX = e.getX();
-		circleY = e.getY();
-		circleR = 0;
-		clicked = true;
+		if(circleR == 0) {
+			circleX = e.getX();
+			circleY = e.getY();
+			circleR = 0;
+			clicked = true;
+		}
 		repaint();
 	}
 
